e2972b7b059a49808e74606342496d6e885e6c34
cauamicael <cauamicael@hotmail.com>
2024-02-17 01:12:59 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let cont = 0;
+const tarefas = document.getElementById("inputTarefa");
+let addTarefa = document.getElementById("add");
+
+
+
+function addTarefa() {
+    let valor = tarefas.ariaValueMax;
+
+    if (valor == null) {
+        console.log("digite algo")
+    }
+
+    return addTarefa;
+}
