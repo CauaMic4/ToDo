fa168d6b3aacdfe79cb1bec6b770afc32b44c466
cauamicael <cauamicael@hotmail.com>
2024-02-20 01:54:27 -0300

    Criação da função que clona os inputs e adição de ação ao btn adicionar

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
 let cont = 0;
 const tarefas = document.getElementById("inputTarefa");
-let addTarefa = document.getElementById("add");
+let AddBtn = document.getElementById("add");
+let camposContainer = document.getElementById("campos-container");
 
-
+addBtn.addEventListener("click", addTarefa);
 
 function addTarefa() {
-    let valor = tarefas.ariaValueMax;
+
+    clonar();
+
+    let valor = tarefas.value;
 
     if (valor == null) {
         console.log("digite algo")
     }
 
     return addTarefa;
+}
+
+function clonar() {
+    let clone = document.querySelector(".campos").cloneNode(true);
+
+    let cloneInput = clone.querySelector("input");
+    cloneInput.disabled = true;
+
+    let toMake = document.querySelector("#toMake")
+    toMake.appendChild(clone);
+
+
 }
