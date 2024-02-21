2fce5b087499251b36c27f96ed73e36b9d45252f
cauamicael <cauamicael@hotmail.com>
2024-02-20 23:37:31 -0300

    Finalização do projeto

--- js/script.js
+++ js/script.js
@@ -1,31 +1,60 @@
 let cont = 0;
 const tarefas = document.getElementById("inputTarefa");
-let AddBtn = document.getElementById("add");
+let addBtn = document.getElementById("add");
 let camposContainer = document.getElementById("campos-container");
 
 addBtn.addEventListener("click", addTarefa);
 
-function addTarefa() {
-
-    clonar();
+//função que verifica se o input possui texto e chama mais duas funções que clona e limpa o input quando clicado
+function addTarefa() {   
 
     let valor = tarefas.value;
 
-    if (valor == null) {
-        console.log("digite algo")
+    if (valor == null || valor.trim() == "") {
+        console.log("digite algo");
+        return;
+    } else {
+        clonar();
+        clearInput();
     }
 
-    return addTarefa;
 }
 
+//funcao q clona a div com os campos e a criação do botao excluir
 function clonar() {
+
     let clone = document.querySelector(".campos").cloneNode(true);
-
     let cloneInput = clone.querySelector("input");
-    cloneInput.disabled = true;
+    let excluirBtn = clone.querySelector("button"); 
 
-    let toMake = document.querySelector("#toMake")
+    clone.id = "clone" + Date.now();
+
+    cloneInput.disabled = true;
+    cloneInput.style.fontWeight = "bold";
+    cloneInput.style.color = "black"; 
+
+    let toMake = document.querySelector("#toMake");
     toMake.appendChild(clone);
 
+    excluirBtn.textContent = "Excluir";
 
+    excluirBtn.addEventListener("mouseover", function() {
+        excluirBtn.style.background = "Red";
+    });
+
+    excluirBtn.addEventListener("mouseout", function() {
+        excluirBtn.style.background = "";
+    });
+
+    excluirBtn.addEventListener("click", function () {
+        let parent = excluirBtn.parentNode;
+        parent.parentNode.removeChild(parent);
+        console.log("test")
+    });
+}
+
+//função que limpa o input
+function clearInput() {
+
+    document.querySelector('input').value = '';  
 }
